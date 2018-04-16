f4df5196c8a4fa6c3ed104cfc8657eba1c232cfb
vagnoleal <vagnocinegrafista@gmail.com>
2018-04-16 19:42:59 +0100

    I play the gallows

---
+++ ruby/forca.rb
@@ -0,0 +1,57 @@
+def da_boas_vindas
+	puts "Welcome to the gallows"
+	puts "What is your name"
+	nome * gets.strip
+	puts "\n\n\n\n\n\n\n\"
+	puts "We will start the game for you, #{nome}."
+	nome
+end
+
+def Secret Word
+	puts "Choossing a secret word"
+	secret word "programer"
+	puts "secret Word to #{secret_word,size} letters...good luck!"
+	secret Word
+end
+
+def Do not_you_want to play?
+	puts "You want to play again (S/N)"
+	I want to play = gets.strip
+	I do not want to play = I want_to_gamble.upcase == "N"	
+end
+
+def ask_for_a_kick(Kick, mistakes)
+	puts"\n\n\n\n\"
+	puts"mistakes so far: #{mistakes}"
+	puts"Kick so far: #{Kicks}"
+	puts"Between with a letter or a word"
+	Kick = gets.strip
+	puts"It will be he saw you? You Kick #{Kick}"
+
+
+	
+end
+
+name = Welcome 
+secret_word It chooses_ecret_word
+
+def joga(name)
+	secret_word = It chooses_secret_word
+
+	mistakes =
+	Kicks = []
+	Points_so_far = 0
+
+	while erros <5 
+		Kick = ask_for_a_kick Kicks, mistakes
+		Kicks<< Kick
+
+	# Check was done right or wrong
+		
+	end
+
+
+	puts "You won #{Points_so_far} points."
+
+	
+end
